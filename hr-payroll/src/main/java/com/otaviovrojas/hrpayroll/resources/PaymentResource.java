538637a0d0540acfaac71902f1905b63e586dcf1
Otavio Vinicius Rojas Godinho Queiroz <otaviovrojas@gmail.com>
2021-08-10 11:19:20 -0300

    rest template

--- hr-payroll/src/main/java/com/otaviovrojas/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/otaviovrojas/hrpayroll/resources/PaymentResource.java
@@ -16,6 +16,7 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable int days) {
         Payment payment = service.getPayment(workerId, days);
--- hr-payroll/src/main/java/com/otaviovrojas/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/otaviovrojas/hrpayroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.otaviovrojas.hrpayroll.services;
 
 import com.otaviovrojas.hrpayroll.entities.Payment;
+import com.otaviovrojas.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workerId, int days){
-        return new Payment("Vinicius", 420.0, days);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    public Payment getPayment(long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
